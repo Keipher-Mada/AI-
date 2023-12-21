53a617ecccdfbd51d2c01987ea862aa8698487b1
Keipher Mada <K.dubber17@gmail.com>
2023-12-21 10:07:03 +0000

    Added js file

---
+++ Script.js
@@ -0,0 +1,78 @@
+const generateForm = document.querySelector(".generate-form");
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "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";
+
+const updatedImageCard = (imgDataArray) => {
+    imgDataArray.forEach((imgObject, index) => {
+        const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+        const imgElement = imgCard.querySelector("img");
+        const downloadBtn = imgCard.querySelector(".download-btn");
+
+        //set the slot to the AI Image
+        const aiGeneratedImg = `data:image/jpeg;based64,${imgObject.b64_json}`;
+        imgElement.src = aiGeneratedImg;
+
+        //remove loading screen when image is loaded
+        imgElement.onload = () => {
+            imgCard.classList.remove("loading");
+            downloadBtn,setAttribute("href",aiGeneratedImg);
+            downloadBtn,setAttribute("download",`${new Data().getTime()}.jpg`);
+
+        }
+    });
+}
+
+//Send requrst to OpenAI API and fatch result
+const generateAiImages = (userPrompt, userImgQuantity) => {
+    try {
+        const responce = await fetch("https://api.openai.com/v1/images/generations", {
+            method: "POST",
+            header: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                 prompt: userPrompt,
+                 n:parseInt(userImgQuantity),
+                 size: "512x512",
+                 responce_format: "b64_json"
+            })
+
+        });
+        
+        if(!responce.ok) throw new Error("Image cannot be facted, plaese try gain later.")
+
+        const{ data } = await responce.json();
+        updatedImageCard([...data]);
+    } catch (error) {
+        console.log(error.message);
+    }  finally {
+        isImageGenerating = false;
+    }
+}
+
+
+
+const handleFormSubmission = (e) => {
+    e.preventDefault();
+
+    //retrived user input
+    const userPrompt = e.srcElement[0].value;
+    const userImgQuantity = e.srcElement[1].value;
+
+    //HTML markup for image in loading state
+    const imgCardMarkup = Array.form({lemgth: userImgQuantity}, () =>
+      `<div class="img-card loading">
+      <img src="images/loader.svg" alt="image">
+      <a href="#" class="download-btn">
+        <img src="images/download.svg" alt="download icon">
+      </a>
+    </div>`
+    ).join("");
+
+    imageGallery.innerHTML = imgCardMarkup;
+    generateAiImages(userPrompt, userImgQuantity);
+}
+
+generateForm.addEventListener("submit", handleFormSubmission);
